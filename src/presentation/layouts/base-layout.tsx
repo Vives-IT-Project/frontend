ba281df641d2b03ddebe78b1ab0a398417a87fac
phllp <felipebeiger@gmail.com>
2025-04-24 21:56:22 +0200

    feat: update base-layout

--- src/presentation/layouts/base-layout.tsx
+++ src/presentation/layouts/base-layout.tsx
@@ -1,51 +1,75 @@
 import { useState } from "react";
 import { Outlet, Link } from "react-router-dom";
-import { Menu } from "lucide-react";
-import home from "../../assets/home.svg";
-import projects from "../../assets/projects.svg";
+import logo from "../../assets/logo.svg";
+import { UserProfileMenu } from "../components/user-profile-menu";
+import { ClipboardList, LayoutDashboard, Settings } from "lucide-react";
+
+type MenuItemProps = {
+  label: string;
+  icon: React.ReactNode;
+  collapsed: boolean;
+  to: string;
+};
+
+const MenuItem: React.FC<MenuItemProps> = ({ label, icon, collapsed, to }) => {
+  return (
+    <Link
+      to={to}
+      className="py-3 w-full text-center text-gray-300 hover:bg-indigo-900 transition-colors "
+    >
+      <div className="flex flex-row justify-center  gap-4">
+        <span>{icon}</span>
+        {!collapsed && <span className="whitespace-nowrap">{label}</span>}
+      </div>
+    </Link>
+  );
+};
 
 const BaseLayout = () => {
   const [isCollapsed, setIsCollapsed] = useState(false);
 
+  const toggleSidebar = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
   return (
     <div className="flex h-screen">
       <aside
-        className={`${
-          isCollapsed ? "w-16" : "w-64"
-        } bg-gray-800 text-white p-4 transition-all duration-300`}
+        className={`bg-indigo-950 text-white flex flex-col items-center transition-all duration-300 ${
+          isCollapsed ? "w-16" : "w-52"
+        }`}
       >
-        <button
-          onClick={() => setIsCollapsed(!isCollapsed)}
-          className="mb-4 p-2 rounded bg-gray-700 hover:bg-gray-600 w-full flex items-center justify-center"
-        >
-          <Menu size={20} />
-        </button>
+        <div className="mt-6 mb-12 cursor-pointer" onClick={toggleSidebar} title="Toggle Sidebar">
+          <div className="w-12 h-12 bg-gray-300 rounded-full flex items-center justify-center">
+            <img src={logo} alt="Logo" />
+          </div>
+        </div>
 
-        <nav>
-          <ul>
-            <li className="py-2 hover:bg-gray-700 rounded px-2 flex items-center">
-              <img src={home} alt="Logo" className="h-5 w-auto" />
-              {!isCollapsed && (
-                <Link to="/" className="ml-2">
-                  Home
-                </Link>
-              )}
-            </li>
-            <li className="py-2 hover:bg-gray-700 rounded px-2 flex items-center">
-              <img src={projects} alt="Logo" className="h-5 w-auto" />
-              {!isCollapsed && (
-                <Link to="/projects" className="ml-2">
-                  Projects
-                </Link>
-              )}
-            </li>
-          </ul>
+        <nav className="flex flex-col items-center w-full gap-2">
+          <MenuItem label="Dashboard" icon={<LayoutDashboard />} collapsed={isCollapsed} to="/" />
+          <MenuItem
+            label="Business Cases"
+            icon={<ClipboardList />}
+            collapsed={isCollapsed}
+            to="/business-cases"
+          />
+          <MenuItem
+            label="Settings"
+            icon={<Settings />}
+            collapsed={isCollapsed}
+            to="/org-settings"
+          />
         </nav>
       </aside>
 
       <div className="flex flex-col flex-1">
-        <header className="bg-gray-900 text-white p-4">
-          <h1 className="text-lg">Portifolio Management</h1>
+        <header className="bg-gray-100 text-gray-900 pt-8 pl-6">
+          <div className="flex justify-between">
+            <h1 className="text-3xl font-semibold">Business Case</h1>
+            <span className="mr-10">
+              <UserProfileMenu />
+            </span>
+          </div>
         </header>
 
         <main className="flex-1 p-6 bg-gray-100">
