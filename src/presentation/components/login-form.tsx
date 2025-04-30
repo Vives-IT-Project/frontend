21d044bef95149ab79a8a9da738580adb8bae596
phllp <felipebeiger@gmail.com>
2025-04-30 09:02:47 +0200

    feat: implement login form

--- src/presentation/components/login-form.tsx
+++ src/presentation/components/login-form.tsx
@@ -1,15 +1,47 @@
 import { useNavigate } from "react-router-dom";
+import api from "../../services/axios";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
 const LoginForm = () => {
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const routeToRegister = () => {
     navigate("/register");
   };
 
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!password.trim()) {
+      toast.error("Password is empty");
+      return;
+    }
+
+    try {
+      const response = await api.post("/users/login", { email, password });
+
+      if (response.status !== 200) {
+        const errorData = await response.data;
+        console.error(errorData);
+        alert(errorData.message || "Registration failed");
+        return;
+      }
+
+      toast.success("Logged In");
+      localStorage.setItem("user", JSON.stringify(await response.data));
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+      toast.error(`Registration error`);
+    }
+  };
+
   return (
     <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm">
-      <form className="space-y-4">
+      <form className="space-y-4" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="email" className="block text-sm font-medium text-gray-700">
             Email
@@ -19,6 +51,9 @@ const LoginForm = () => {
             id="email"
             className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
             placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
           />
         </div>
 
@@ -31,6 +66,9 @@ const LoginForm = () => {
             id="password"
             className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
             placeholder="••••••••"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
           />
         </div>
 
