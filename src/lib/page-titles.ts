0b51a9493f20f8590a76ab55aa3abb97ab06aaf0
phllp <felipebeiger@gmail.com>
2025-05-12 20:15:55 +0200

    feat: add dinamic page title

---
+++ src/lib/page-titles.ts
@@ -0,0 +1,10 @@
+export const pageTitles: Record<string, string> = {
+  "/": "Home",
+  "/business-cases": "Business Cases",
+  "/manage-templates": "Business Cases Templates",
+  "/new-business-case": "New Business Case",
+  "/new-template": "New Template",
+  "/app-settings": "Settings",
+  "/login": "Login",
+  "/register": "Register",
+};
